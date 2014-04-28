3ba23f437352b1a9a8ba623a3cb4cca528dc8a57
Adrian <macylapka1@gmail.com>
2014-04-28 06:43:04 +0200

    update increase array length

--- include/rle.h
+++ include/rle.h
@@ -3,9 +3,21 @@
 
 class run_length_encoding {
 public:
-  static bool decode(char unsigned*, char unsigned*, int unsigned&, int unsigned&);
+  bool static decode(char unsigned*, int unsigned&, 
+    char unsigned*, int unsigned&);
 private:
-  run_length_encoding(); 
+  int static array_size;
+  int static position;
+  int const static increase_size = 1000;
+ 
+  // pobieramy adres wskaźnika na tablice 
+  // i powiekszamy ją o podany rozmiar
+  // zwracamy rozmiar powiększonej tablicy
+  int static increase_array_size(
+    char unsigned **input_array, 
+    int unsigned length, 
+    int unsigned size);
+  run_length_encoding();
 };
 
 #endif /*RLE_H*/
