b83a50228c4a43e21af664249ff6f7f265129bf2
Adrian <macylapka1@gmail.com>
2014-05-05 06:32:00 +0200

    change return value and arguments

--- include/file_loader.h
+++ include/file_loader.h
@@ -3,11 +3,13 @@
 
 #include <string>
 #include <fstream>
+#include <memory>
 
 class file_loader {
 
 public:
-  int unsigned static get_data(std::string&, char*);
+  std::unique_ptr<char[]> static get_data(
+    std::string&, int unsigned&);
   file_loader() = delete;
   file_loader &operator=(file_loader const&) = delete;
 };
--- src/file_loader.cpp
+++ src/file_loader.cpp
@@ -1,18 +1,18 @@
 #include <file_loader.h>
 
-unsigned int file_loader::get_data(std::string &full_file_path,
-    char *buffer) {
+std::unique_ptr<char[]> file_loader::get_data(std::string &full_file_path,
+    int unsigned &length) {
   std::ifstream input_file_stream(full_file_path.c_str(), 
     std::ifstream::binary);
   if(input_file_stream.is_open()) {
     input_file_stream.seekg(0, input_file_stream.end);
-    int length = input_file_stream.tellg();
+    length = input_file_stream.tellg();
     input_file_stream.seekg(0, input_file_stream.beg);
     
-    buffer = new char[length];
-    input_file_stream.read(buffer, length);
+    std::unique_ptr<char[]> data(new char[length]);
+    input_file_stream.read(data.get(), length);
     input_file_stream.close();
-    return length; 
+    return data; 
   } 
   throw "Unable load file";
 }
